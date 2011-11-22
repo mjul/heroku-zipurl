712f62bc7c74cf490b4c0f2c377e9216a20a721c
Martin Jul <martin@mjul.com>
2011-11-22 17:46:52 +0100

    Heroku example app.

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject zipurl "1.0.0"
+  :description "Heroku Clojure web app: fetch and zip data at a given URL."
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [ring/ring-jetty-adapter "0.3.9"]])
---
+++ test/zipurl/test/core.clj
@@ -0,0 +1,6 @@
+(ns zipurl.test.core
+  (:use [zipurl.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
