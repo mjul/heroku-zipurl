d6c16ab02f76605f9116a4c0685807c4b6b2143d
Martin Jul <martin@mjul.com>
2011-11-23 22:40:06 +0100

    Added feature to download the contents of a URL in a zip file.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject zipurl "1.0.0"
             :description "Clojure web app for Heroku"
-            :dependencies [[org.clojure/clojure "1.2.1"]
-                           [noir "1.2.1"]]
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.1"]
+                           [clj-http "0.2.4"]]
             :main zipurl.server)
 
--- src/zipurl/views/welcome.clj
+++ src/zipurl/views/welcome.clj
@@ -1,10 +1,50 @@
 (ns zipurl.views.welcome
+  (:import [java.io ByteArrayOutputStream]
+           [java.util.zip ZipOutputStream ZipEntry])
   (:require [zipurl.views.common :as common]
-            [noir.content.getting-started])
+            [noir.content.getting-started]
+            [clj-http.client :as client])
   (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
+        [hiccup.core :only [html]]
+        [clojure.java.io :only [reader writer copy file input-stream]]
+        ))
 
 
 (defpage "/" []
   (common/layout
-   [:p "Welcome to ZIP URL"]))
+   [:h1 "Choose a URL to ZIP and download"]
+   [:form {:action "/download/" :method "GET"}
+    [:input {:name "url" :type "text" :id "url"}]
+    [:input {:type "Submit" :value "Download as zip"}]]
+   ))
+
+
+(defn download [url]
+  (let [resp (client/get url {:as :byte-array})]
+    (when (= 200 (:status resp))
+      (:body resp))))
+
+
+(defn create-zip [data]
+  (let [bos (ByteArrayOutputStream.)
+        zipfile (ZipOutputStream. bos)
+        zipentry (ZipEntry. "content.dat")]
+    (.setSize zipentry (count data))
+    (doto zipfile
+      (.putNextEntry zipentry)
+      (.write data 0 (count data))
+      (.closeEntry)
+      (.close))
+    (.toByteArray bos)))
+
+
+(defpage "/download/" {:keys [url]}
+  (if-let [bytes (download url)]
+    {:header {:content-type "application/zip"
+              :content-length (count bytes)}
+     :body (input-stream (create-zip bytes))}
+    (common/layout
+     [:h1 "Download failed."])))
+
+    
+
